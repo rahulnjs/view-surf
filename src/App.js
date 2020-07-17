286b75994214bfc0422f8b04f7872ae9a8f62356
aniketaman <aniket.aman007@gmail.com>
2020-07-17 14:47:06 +0530

    created state for html and css and passed to child component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Editor from './component/Editor';
 import HTMLOutput from './component/HTMLOutput';
 import css from './app.scss';
@@ -6,18 +6,35 @@ import css from './app.scss';
 function App() {
 
   var _log = window.console.log;
+  const dumpRef = useRef();
+  const [html, setHtml] = useState(``);
+  const [css, setCss] = useState(``);
 
   window.console.log = dump;
-
-
   function dump(l) {
     document.getElementById('console-dump').innerHTML += '<div class="op">' + l + '</div>';
   }
 
+  function setOutput({html, css}) {
+
+    if(html) {
+      setHtml(html)
+    }
+
+    if(css) {
+      console.log(css)
+      setCss(css);
+    } else {
+      setCss('')
+    }
+  }
+
+
+
   return (
     <div className="app">
-        <Editor />
-        <HTMLOutput />
+        <Editor onRender={setOutput}/>
+        <HTMLOutput render={html} css={css}/>
     </div>
   );
 }
